5a181c23598e2eabdb3a15eea29a5e915061b901
Eric Ferguson <turtle0888@gmail.com>
2019-12-03 18:03:42 -0600

    Add axios calls to App.js

--- react-github-user-card/src/App.js
+++ react-github-user-card/src/App.js
@@ -1,13 +1,36 @@
 import React from 'react';
 import './App.css';
+import axios from 'axios';
+import UserCard from './components/UserCard';
 
 class App extends React.Component {
+  state = {
+      user: {},
+      followers: []
+    }
+  
+  
+
+ componentDidMount(){
+   axios.get('https://api.github.com/users/EricFerguson76')
+   .then(response => {
+     console.log(response)
+     this.setState({user: response.data})
+   });
+   axios.get('https://api.github.com/users/EricFerguson76/followers')
+   .then(response => {
+     console.log(response)
+     this.setState({followers: response.data})
+   })
+ };
 
 
 
   render(){
   return (
     <div className="App">
+      <h1>GitHub User Cards</h1>
+      <UserCard user ={this.state.user} />
       
     </div>
   );
